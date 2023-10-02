7e4540ff938f3f2a91f50860497dfbd2ce2ce3b6
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2023-10-02 14:57:30 +0200

    Init vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center bg-slate-600">
+        <h1 className="text-3xl text-white">PRJ3</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
