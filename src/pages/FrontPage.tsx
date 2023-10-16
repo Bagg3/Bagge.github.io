282f618c2bbad37130a3c3415f9d8145efd5e33b
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-10-16 23:12:14 +0200

    Removed top as it wasnt used

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 const FrontPage = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-600">
-      <h1 className="top-56 mb-10 text-6xl font-bold text-white">
-        Hestevæddeløbet
-      </h1>
+      <h1 className="mb-14 text-6xl font-bold text-white">Hestevæddeløbet</h1>
       <Link to="/setup">
         <button className="mb-10 h-16 w-64 rounded-full bg-slate-400 text-2xl font-bold text-white shadow-lg hover:bg-slate-300 active:bg-slate-200">
           Spil Offline
