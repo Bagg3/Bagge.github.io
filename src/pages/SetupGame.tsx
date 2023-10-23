749d0c04d75272eb093d32cc965af5daf8a8d1cf
Bagg3 <andreasbaggenielsen@gmail.com>
2023-10-23 12:22:01 +0200

    Chagned file location

--- src/pages/SetupGame.tsx
+++ src/pages/SetupGame.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import CustomDropdown from '../utility/CustomDropdown';
+import CustomDropdown from '../components/CustomDropdown';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const SetupGame = () => {
